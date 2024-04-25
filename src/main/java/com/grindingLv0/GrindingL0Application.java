376cb07b812fa48af422f4df6408c52a74d2d418
Jeffershonq <jeffershon.jdp@gmail.com>
2024-04-25 23:03:49 +0700

    day2

---
+++ src/main/java/com/grindingLv0/GrindingL0Application.java
@@ -0,0 +1,13 @@
+package com.grindingLv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrindingL0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrindingL0Application.class, args);
+	}
+
+}
